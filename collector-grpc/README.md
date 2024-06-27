1092c0d98fda006fec10eedef29b6bfc18d882e5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2024-06-27 02:14:18 +0200

    Removing GRPC (#3)

--- collector-grpc/README.md
+++
@@ -1,5 +0,0 @@
-# zipkin-collector-otel-grpc
-
-This component implements
-the [OTLP/gRPC protocol](https://opentelemetry.io/docs/specs/otlp/#otlpgrpc)
-with [Armeria](https://armeria.dev/).
--- module/src/main/java/zipkin/module/otel/ZipkinOpenTelemetryHttpCollectorModule.java
+++ module/src/main/java/zipkin/module/otel/ZipkinOpenTelemetryHttpCollectorModule.java
@@ -20,7 +20,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import zipkin2.collector.CollectorMetrics;
 import zipkin2.collector.CollectorSampler;
-import zipkin2.collector.otel.grpc.OpenTelemetryGrpcCollector;
 import zipkin2.collector.otel.http.OpenTelemetryHttpCollector;
 import zipkin2.storage.StorageComponent;
 
